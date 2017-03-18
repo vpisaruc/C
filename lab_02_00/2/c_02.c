a04c38484364cb09fafd51be8495f3434d746eb9
U-VLAB\student <student@IU7-01-12.vlab.bmstu.ru>
2017-03-18 15:11:04 +0300

    First and second programm done

--- lab_02_00/2/c_02.c
+++ lab_02_00/2/c_02.c
@@ -3,15 +3,30 @@
 
 float main(void)
 {
-	float x_a,x_b,x_c,y_a,y_b,y_c,s1,s2,s3;
-	printf("Pervuiu vershinu treugolinika: ")
-	scanf("%f%f%f",&x_a, &y_a)
-	printf("Pervuiu vershinu treugolinika: ")
-	scanf("%f%f%f",&x_b, &y_b)
-	printf("Pervuiu vershinu treugolinika: ")
-	scanf("%f%f%f",&x_c, &y_c)
+	float x_a, x_b, x_c, y_a, y_b, y_c, s1, s2, s3;
+	printf("Pervuiu vershinu treugolinika: ");
+	scanf("%f%f%f", &x_a, &y_a);
+	printf("Vtoruiu vershinu treugolinika: ");
+	scanf("%f%f%f", &x_b, &y_b);
+	printf("Tretiu vershinu treugolinika: ");
+	scanf("%f%f%f", &x_c, &y_c);
 	
-	s1 = abs(sqrt(pow((x_b-x_a),2)) + sqrt(pow((y_b-y_a),2) )
-	s2 = abs(sqrt(pow((x_c-x_a),2)) + sqrt(pow((y_c-y_a),2) )
-	s3 = abs(sqrt(pow((x_c-x_b),2)) + sqrt(pow((y_c-y_b),2) )
+	s1 = abs(sqrt(pow((x_b - x_a),2)) + sqrt(pow((y_b - y_a), 2) ));
+	s2 = abs(sqrt(pow((x_c - x_a),2)) + sqrt(pow((y_c - y_a), 2) ));
+	s3 = abs(sqrt(pow((x_c - x_b),2)) + sqrt(pow((y_c - y_b), 2) ));
+	
+	if ((s1 + s2) <= s3){
+		printf("It's not triangle.");
+	}
+	else if (s3 = sqrt(pow(s1, 2) + pow(s2, 2))){
+		printf("This is rectangular triangle");
+	}
+	else if ((pow(s1, 2) + pow(s2, 2) - pow(s3, 2))/(2*s1*s2)>0){
+		printf("This is acute triangle");
+	}
+	else{
+		printf("This is obtuse triangle");
+	}
+	return 0;
+
 }
