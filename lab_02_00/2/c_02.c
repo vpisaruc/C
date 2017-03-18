f8acd96e5015f2a58753940f64a1b3542bfd0aeb
U-VLAB\student <student@IU7-01-12.vlab.bmstu.ru>
2017-03-18 14:48:26 +0300

    lab_02_00 was added

---
+++ lab_02_00/2/c_02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+float main(void)
+{
+	float x_a,x_b,x_c,y_a,y_b,y_c,s1,s2,s3;
+	printf("Pervuiu vershinu treugolinika: ")
+	scanf("%f%f%f",&x_a, &y_a)
+	printf("Pervuiu vershinu treugolinika: ")
+	scanf("%f%f%f",&x_b, &y_b)
+	printf("Pervuiu vershinu treugolinika: ")
+	scanf("%f%f%f",&x_c, &y_c)
+	
+	s1 = abs(sqrt(pow((x_b-x_a),2)) + sqrt(pow((y_b-y_a),2) )
+	s2 = abs(sqrt(pow((x_c-x_a),2)) + sqrt(pow((y_c-y_a),2) )
+	s3 = abs(sqrt(pow((x_c-x_b),2)) + sqrt(pow((y_c-y_b),2) )
+}
