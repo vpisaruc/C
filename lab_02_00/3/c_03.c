addeb13ffb33d94e37162ca6bc9a692eb4798b5d
Pisaruc Victor <vpisaruc@gmail.com>
2017-03-23 13:09:27 +0300

    Update c_03.c

--- lab_02_00/3/c_03.c
+++ lab_02_00/3/c_03.c
@@ -4,16 +4,16 @@
 static float
 factorial (int n)
 {
-  float r;
-  for (r = 1; n > 1; r *= (n--));
-  return r;
+    float r;
+    for (r = 1; n > 1; r *= (n--));
+    return r;
 }
 
 
 
 float main(void)
 {
-	float x, eps, s, z, k, f, ab, otn;
+    float x, eps, s, z, k, f, ab, otn;
 	printf("\n\nInput X: ");
 	scanf("%f", &x);
 	printf("\n\nInput epsilion: ");
@@ -28,7 +28,7 @@ float main(void)
 	}
         printf("\n\nSumma reada s tocinostiu %f ravna %f", eps, s);	
 	f = exp(x);
-	printf("\n\ne ^ x = %f ", f);
+    printf("\n\ne ^ x = %f ", f);
 	ab = fabs(f - s);
 	otn = fabs((f - s)/f);
 	printf("\n\nAbsoliutnaia oshibka: %f", ab);
