d6d6e28d1700b9b451947ae745bae6edac67a5d5
pv16u913 <vpisaruc@gmail.com>
2017-03-23 13:20:56 +0300

    Count of spaces in 3rd programm  was fixed_2

--- lab_02_00/3/c_03.c
+++ lab_02_00/3/c_03.c
@@ -4,16 +4,6 @@
 static float
 factorial (int n)
 {
-    float r;
-    for (r = 1; n > 1; r *= (n--));
-    return r;
-}
-#include <stdio.h>
-#include <math.h>
-
-static float
-factorial (int n)
-{
   float r;
   for (r = 1; n > 1; r *= (n--));
   return r;
@@ -48,34 +38,5 @@ float main(void)
     printf("\n\n");	
 	
     return 0;
-
-}
-
-
-float main(void)
-{
-    float x, eps, s, z, k, f, ab, otn;
-	printf("\n\nInput X: ");
-	scanf("%f", &x);
-	printf("\n\nInput epsilion: ");
-	scanf("%f", &eps);
-	s = 1;
-	z = x;
-	k = 1;
-	while (z > eps){
-	    z = pow(x, k)/factorial(k);
-	    s += z;
-	    k += 1;
-	}
-        printf("\n\nSumma reada s tocinostiu %f ravna %f", eps, s);	
-	f = exp(x);
-    printf("\n\ne ^ x = %f ", f);
-	ab = fabs(f - s);
-	otn = fabs((f - s)/f);
-	printf("\n\nAbsoliutnaia oshibka: %f", ab);
-	printf("\n\nOtnositelinaia oshibka: %f", otn);
-	printf("\n\n");	
-	
-	return 0;
 
 }
