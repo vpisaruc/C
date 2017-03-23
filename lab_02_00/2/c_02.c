7b12716ceb4956b7b478e57642e0ebe0dd703125
pv16u913 <vpisaruc@gmail.com>
2017-03-23 13:03:04 +0300

    3rd programm was edded, error fixed in programm 2

--- lab_02_00/2/c_02.c
+++ lab_02_00/2/c_02.c
@@ -11,21 +11,21 @@ float main(void)
 	printf("Tretiu vershinu treugolinika: ");
 	scanf("%f%f", &x_c, &y_c);
 	
-	s1 = abs(sqrt(pow((x_b - x_a),2)) + sqrt(pow((y_b - y_a), 2) ));
-	s2 = abs(sqrt(pow((x_c - x_a),2)) + sqrt(pow((y_c - y_a), 2) ));
-	s3 = abs(sqrt(pow((x_c - x_b),2)) + sqrt(pow((y_c - y_b), 2) ));
+	s1 = fabsf(sqrt(pow((x_b - x_a),2)) + sqrt(pow((y_b - y_a), 2) ));
+	s2 = fabsf(sqrt(pow((x_c - x_a),2)) + sqrt(pow((y_c - y_a), 2) ));
+	s3 = fabsf(sqrt(pow((x_c - x_b),2)) + sqrt(pow((y_c - y_b), 2) ));
 	
 	if ((s1 + s2) <= s3){
-		printf("It's not triangle.");
+		printf("It's not triangle.\n\n");
 	}
 	else if (s3 = sqrt(pow(s1, 2) + pow(s2, 2))){
-		printf("This is rectangular triangle");
+		printf("This is rectangular triangle\n\n");
 	}
 	else if ((pow(s1, 2) + pow(s2, 2) - pow(s3, 2))/(2*s1*s2)>0){
-		printf("This is acute triangle");
+		printf("This is acute triangle\n\n");
 	}
 	else{
-		printf("This is obtuse triangle");
+		printf("This is obtuse triangle\n\n");
 	}
 	return 0;
 
---
+++ lab_02_00/3/c_03.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+static float
+factorial (int n)
+{
+  float r;
+  for (r = 1; n > 1; r *= (n--));
+  return r;
+}
+
+
+
+float main(void)
+{
+	float x, eps, s, z, k, f, ab, otn;
+	printf("\n\nInput X: ");
+	scanf("%f", &x);
+	printf("\n\nInput epsilion: ");
+	scanf("%f", &eps);
+	s = 1;
+	z = x;
+	k = 1;
+	while (z > eps){
+	    z = pow(x, k)/factorial(k);
+	    s += z;
+	    k += 1;
+	}
+        printf("\n\nSumma reada s tocinostiu %f ravna %f", eps, s);	
+	f = exp(x);
+	printf("\n\ne ^ x = %f ", f);
+	ab = fabs(f - s);
+	otn = fabs((f - s)/f);
+	printf("\n\nAbsoliutnaia oshibka: %f", ab);
+	printf("\n\nOtnositelinaia oshibka: %f", otn);
+	printf("\n\n");	
+	
+	return 0;
+
+}
