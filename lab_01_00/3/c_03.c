59df767430c1b1673358614dbbde2d4ab71bcd0c
U-VLAB\user-lab01 <user-lab01@LAB-01-56.vlab.bmstu.ru>
2017-03-25 14:22:07 +0300

    Dobavlen perevod iz santimetrov v metri i ispravlena vtoraia formula

--- lab_01_00/3/c_03.c
+++ lab_01_00/3/c_03.c
@@ -1,16 +1,18 @@
 //Определить нормальный вес человека и индекс массы его тела по формулам: h * t / 240 и m / h 2, где h – рост человека (измеряемый в сантиметрах в первой формуле и в метрах –во второй); t – длина окружности грудной клетки (в сантиметрах); m – вес (вкилограммах).
 
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    float h, m, t, f, g;
-    printf("Enter height,weight and length chest: ");
+    float h, m, t, f, g, h1;
+    printf("Enter height(in centimeters ),weight and length chest: ");
     scanf("%f%f%f", &h, &m, &t);
+	h1 = h / 100;
     f=(h * t) / 240;
-    g=m / (h * h);
-    printf("Normal weight: %f \n", f );
-    printf("Weight index: %f \n", g);
+    g= m / pow(h1, 2);
+    printf("\n\nNormal weight: %f \n", f );
+    printf("\n\nWeight index: %f \n", g);
 
     return 0; 
     
