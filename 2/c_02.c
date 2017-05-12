91b17a3362c0cb22581600fcbd10b7cbf78a5ec6
pv16u913 <vpisaruc@gmail.com>
2017-05-12 23:14:20 +0300

    Created 2 programms

--- 2/c_02.c
+++ 2/c_02.c
@@ -1,41 +1,102 @@
 #include <stdio.h>
+#include <math.h>
 
 
-float average_value(FILE *f, int *sr)
+#define OK 0
+#define WRONG_INPUT_1 -1
+#define WRONG_INPUT_2 -2
+#define FILE_ERROR -3
+#define EMPTY_FILE -4
+
+
+float average_value(FILE *file, float *middle)
 {
-    int num,s,k;
-    s = 0;
+    float num_1,s,k;
     k = 0;
-    if (fscanf(f, "%d", sr) == 1)
+    
+    if (fscanf(file, "%f", middle) == 1)
     {
-        while (fscanf(f, "%d", &num) == 1)
+        while (fscanf(file, "%f", &num_1) == 1)
         {
-            s += num;
+            s += num_1;
             k += 1;
-            return 0;
+            
         }
-    *sr = s/k;
+
+
+
+        *middle = s/k;
+
+        return OK;
+    
     }
- return -1;
+    return WRONG_INPUT_1;
 }
 
 
 
-int main(void)
+float number_near_middle(FILE *file, float *true_number, float *md )
 {
-    FILE *f;
-    float sr;
+    float minus_1, minus_2, num_2;
+    //int num_2;
 
-    f = fopen("test.txt", "r");
-    if (f == NULL)
+
+
+    if (fscanf(file, "%f", true_number) == 1)
     {
-    printf("I/O error\n");
-    return -1;
+        minus_1 = fabs(*true_number - *md);
+        while (fscanf(file, "%f", &num_2) == 1)
+        {
+            minus_2 = fabs(num_2 - *md);
+            if (minus_1 > minus_2)
+            {
+                *true_number = num_2;
+                minus_1 = minus_2;
+            }
+
+        }
+        return OK;
     }
-    if (average_value(f,&sr) == 0)
-        printf("Average value is %f\n", sr);
+    return WRONG_INPUT_2;
+}
+
+
+
+int main(int argc, char **argv)
+{
+    FILE *file;
+    float md, middle,true_number;
+    //int true_number;
+
+    file = fopen(argv[argc-1], "r");
+
+
+    if (file == NULL)
+    {
+        printf("I/O error\n");
+        return FILE_ERROR;
+    }
+
+
+
+
+
+    if (average_value(file, &middle) == 0)
+    {
+
+        md = middle;
+        printf("Average value is %f\n", md);
+        rewind(file);
+        if ((number_near_middle(file, &true_number, &md)) == 0)
+        {
+            printf("The nearest number to the average value is %f\n",true_number);
+            fclose(file);
+            return OK;
+        }
+    }
     else
+    {
         printf("There are not enough data or file is empty.\n");
-    fclose(f);
-    return 0;
+        return EMPTY_FILE;
+    }
 }
