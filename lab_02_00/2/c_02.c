ad81ce3bea6bd95f8bbfccfb45c4c9b76badb87c
Pisaruc Victor <vpisaruc@gmail.com>
2017-05-13 15:06:58 +0300

    Update c_02.c

--- lab_02_00/2/c_02.c
+++ lab_02_00/2/c_02.c
@@ -9,6 +9,7 @@
 #define RECTAGULAR 1
 #define ACUTE 2
 #define OBTUSE 3
+#define TRIANGLE_ERROR -5
 #define OK 0
 
 float triangle(float x_a, float x_b, float x_c, float y_a, float y_b, float y_c)
@@ -16,18 +17,14 @@ float triangle(float x_a, float x_b, float x_c, float y_a, float y_b, float y_c)
     float s1, s2, s3, s1_2, s2_2, s3_2;
     int n;
     //Находим квадраты сторон	
-	s1_2 = pow((x_a - x_b), 2) + pow((y_a - y_b), 2);
+    s1_2 = pow((x_a - x_b), 2) + pow((y_a - y_b), 2);
 	s2_2 = pow((x_b - x_c), 2) + pow((y_b - y_c), 2);
 	s3_2 = pow((x_c - x_a), 2) + pow((y_c - y_a), 2);
-
-    //Находим стороны
-    s1 = sqrt(s1_2);
-    s2 = sqrt(s2_2);
-    s3 = sqrt(s3_2);
+	
+	
 
     //Проверка на существование треугольника	
     if (((s1_2 + s2_2) > s3_2) || ((s1_2 + s3_2) > s2_2) || ((s2_2 + s3_2) > s1_2))
-    
     {
         //Проверка является ли треугольник прямоугольным
         if ((s3_2 == s1_2 + s2_2) || (s2_2 == s1_2 + s3_2) || (s1_2 == s3_2 + s2_2))
@@ -35,7 +32,7 @@ float triangle(float x_a, float x_b, float x_c, float y_a, float y_b, float y_c)
             return RECTAGULAR;
         }
         //Проверка является ли треугольник остроугольным
-        else if (((s1_2 + s2_2 - s3_2)/(2*s1*s2) > 0) && ((s3_2 + s2_2 - s1_2)/(2*s3*s2)>0) && ((s1_2 + s3_2 - s2_2)/(2*s1*s3)>0))
+        else if ((s1_2 + s2_2) > s3_2) || ((s1_2 + s3_2) > s2_2) || ((s3_2 + s2_2) > s1_2))
         {
             return ACUTE;
         }
@@ -47,8 +44,7 @@ float triangle(float x_a, float x_b, float x_c, float y_a, float y_b, float y_c)
     }
     else
     {
-	    printf("This is not triangle \n\n");
-		return ERROR_CODE;
+		return TRIANGLE_ERROR;
     }
     return OK;
 }
@@ -68,18 +64,23 @@ int main(void)
             if(scanf("%f %f", &x_c, &y_c) == 2)
 			{
 				n = triangle(x_a, x_b, x_c, y_a, y_b, y_c);
-                if (n == 1)
+				if (n == TRIANGLE_ERROR)
+                {
+                    printf("This is not triangle \n");
+                }
+                if (n == RECTAGULAR)
                 {
                     printf("This is rectagular triangle \n");
                 }
-                if (n == 2)
+                if (n == ACUTE)
                 {
                     printf("This is acute triangle \n");
                 }
-                if (n == 3)
+                if (n == OBTUSE)
                 {
                     printf("This is obtuse triangle \n");
                 }
+                
 			}
 			else
 			{
