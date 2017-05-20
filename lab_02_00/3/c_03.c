3bbb7e0427e83919e8a2aeced505600ceddd4e7a
Pisaruc Victor <vpisaruc@gmail.com>
2017-05-20 17:27:50 +0300

    Update c_03.c

--- lab_02_00/3/c_03.c
+++ lab_02_00/3/c_03.c
@@ -17,9 +17,9 @@ float sum(int x,  float eps)
     
     while (z > eps)
     {
-	    z = z * x/i;
-	    s += z;
-	    i += 1;
+        z = z * x/i;
+        s += z;
+        i += 1;
     }
     return s;
 }
@@ -32,21 +32,21 @@ int main(void)
     float x, eps, ab, otn;
     printf("\n\nInput X: ");
     if(scanf("%f", &x) !=1)
-	{
-		printf("I/O error\n");
-		return WRONG_INPUT_1;
-	}
+    {
+        printf("I/O error\n");
+        return WRONG_INPUT_1;
+    }
     printf("\n\nInput epsilion: ");
     if(scanf("%f", &eps) != 1)
-	{
-		printf("I/O error\n");
-		return WRONG_INPUT_2;
-	}
-	if (eps <= 0)
-	{
-	    printf("Eps could't be 0 or negative");
-	    return NEGATIVE_EPS;
-	}
+    {
+        printf("I/O error\n");
+        return WRONG_INPUT_2;
+    }
+    if (eps <= 0)
+    {
+        printf("Eps could't be 0 or negative");
+        return NEGATIVE_EPS;
+    }
     printf("\n\nSumma reada s tocinostiu %f ravna %f", eps, sum(x,eps));	
     printf("\n\ne ^ x = %f ", exp(x));
     ab = fabs(exp(x) - sum(x,eps));
@@ -54,7 +54,7 @@ int main(void)
     printf("\n\nAbsoliutnaia oshibka: %f", ab);
     printf("\n\nOtnositelinaia oshibka: %f", otn);
     printf("\n\n");	
-	
+
     return OK;
 
 }
