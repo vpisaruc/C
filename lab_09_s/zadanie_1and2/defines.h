5d9426718c2a919f63974f3326c191e02250ab0a
Pisaruc Victor <vpisaruc@gmail.com>
2017-10-11 13:31:36 +0300

    Zadanie 1 i 2 sokrashionnaia laba

---
+++ lab_09_s/zadanie_1and2/defines.h
@@ -0,0 +1,9 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define OK 0
+#define ERR_IO -1
+#define DELIMS " !.,?\n"
+#define NO_WORDS -2
+
+#endif
---
+++ lab_09_s/zadanie_1and2/str.h
@@ -0,0 +1,12 @@
+#ifndef STR_H
+#define STR_H
+
+int count_words(const char *str);
+
+void split_words(char *str, char *words[]);
+
+void print_words(char *words[], int *n);
+
+void sort_str(char *words[], int *n);
+
+#endif
