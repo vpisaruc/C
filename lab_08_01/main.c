8e1fb6aaa157d17ba9e25cad954a9c7f42e016fb
Pisaruc Victor <vpisaruc@gmail.com>
2017-12-07 02:16:55 +0300

    Update main.c

--- lab_08_01/main.c
+++ lab_08_01/main.c
@@ -64,8 +64,9 @@ int main(int argc, char **argv)
                             file3 = fopen(argv[argc - 1], "w");
 
                             //Matrix_3 is summ of matrix_1 and matrix_2
+                            /*
                             matrix_3 = MatrixSumm(row_1, column_1, row_2, column_2, matrix_1, matrix_2);
-
+                                */
                             //Error analization
                             if(matrix_3 == NULL)
                             {
@@ -105,9 +106,9 @@ int main(int argc, char **argv)
                             FILE *file3 = NULL;
                             file3 = fopen(argv[argc - 1], "w");
                             //Multiply of matrix_1 and matrix_2
-                            /*
+                            
                             matrix_3 = MatrixMult(row_1, column_1, row_2, column_2, matrix_1, matrix_2);
-                            */
+                            
                             if(matrix_3 == NULL)
                             {
                                 printf("\n\nCounts of colums of first matrix must be simmilar as counts of rows in second matrix.\n\n");
