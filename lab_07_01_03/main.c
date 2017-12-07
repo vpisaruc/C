9dc226a353b0d8859b473bd254bbf0c728dbd190
Pisaruc Victor <vpisaruc@gmail.com>
2017-12-08 00:53:13 +0300

    Update main.c

--- lab_07_01_03/main.c
+++ lab_07_01_03/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
     FILE * file = NULL;
     int retVal, cntElem, cntWorkElem;
-    int *arrInp = NULL, *arrWork = NULL, *afterLastElem = NULL, *lastPrintElem = NULL, *idxWork = NULL, *idxInp = NULL;
+    int *arrInp = NULL, *arrWork = NULL, *afterLastElem = NULL, *lastPrintElem = NULL, *afterLastElemWork = NULL;
     //unsigned long long tb_mySort, te_mySort, tb_qSort, te_qSort;
     
     
@@ -47,30 +47,25 @@ int main(int argc, char **argv)
         exit(retVal);
     }
 
-    // Выделение памяти
+    // ��������� ������
     arrInp = (int*)malloc(cntElem * sizeof(int));
     if (!arrInp)
     {
         printf("\nMemory allocation error\n");
-        return MEMORY_ERROR;
-    }
-    afterLastElem = arrInp + cntElem;
-    arrWork = (int*)malloc(cntElem * sizeof(int));
-    if (!arrWork)
-    {
-        free(arrInp);
-        printf("\nMemory allocation error\n");
-        return MEMORY_ERROR;
+        exit(MEMMORY_ERROR);
     }
     
+    afterLastElem = arrInp + cntElem;
+    arrWork = arrInp;
+    afterLastElemWork = afterLastElem;
+
     // �������� ������
     retVal = loadFileData(file, arrInp, &cntElem);
     if (retVal < 0)
     {
-        // Вовзрат кода ошибки
-        free(arrInp);
-        free(arrWork);
+        // ������� ���� ������
         printf("Error = %d\n\nInput any key for Exit", retVal);
+        
 
         exit(retVal);
     }
@@ -87,64 +82,38 @@ int main(int argc, char **argv)
     {
         if (*argv[argc - 1] == 'f')
         {
-            int **arrWorkPtr = NULL, **arrWorkAfterPtr = NULL;
-            arrWorkPtr = (int**)malloc(sizeof(int));
-            arrWorkAfterPtr = (int**)malloc(sizeof(int));
-            *arrWorkPtr = arrWork;
-            *arrWorkAfterPtr = *arrWorkPtr + cntElem;
-            retVal = key(arrInp, afterLastElem, arrWorkPtr, arrWorkAfterPtr);
-            if (retVal == MEMORY_ERROR)
+            retVal = key(arrInp, afterLastElem, &arrWork, &afterLastElemWork);
+            if (retVal == MEMMORY_ERROR)
             {
                 printf("\nMemory error\n");
-                free(arrWorkPtr);
-                free(arrWorkAfterPtr);
-                exit(MEMORY_ERROR);
+                exit(MEMMORY_ERROR);
             }
             if (retVal == INCORRECT_PARAM)
             {
                 printf("\nIncorrect parametres\n");
-                free(arrWorkPtr);
-                free(arrWorkAfterPtr);
                 exit(INCORRECT_PARAM);
             }
-
             if (retVal == NONE_ELEMENTS)
             {
                 printf("\nNone elements before negative\n");
-                free(arrWorkPtr);
-                free(arrWorkAfterPtr);
                 exit(NONE_ELEMENTS);
             }
-
             if (retVal == SIZE_ERROR)
             {
                 printf("Size error");
-                free(arrWorkPtr);
-                free(arrWorkAfterPtr);
                 exit(SIZE_ERROR);
             }
 
-            // Присвоение данных из функции
-            cntWorkElem = *arrWorkAfterPtr - *arrWorkPtr;
+            cntWorkElem = afterLastElemWork - arrWork;
 
             // ������ ���������������� �������
             lastPrintElem = arrWork + cntWorkElem;
             printArray("Filtered Array", arrWork, lastPrintElem);
-
-            free(arrWorkPtr);
-            free(arrWorkAfterPtr);
         }
     }
     else
     {
-        // Без фильтрации
-        idxInp = arrInp;
-        idxWork = arrWork;
-        // Перезапись массива
-        for (int i = 0; i < cntElem; i++)
-        {
-            *idxWork++ = *idxInp++;
-        }
+        // ��� ����������
         cntWorkElem = cntElem;
     }
 
@@ -167,13 +136,13 @@ int main(int argc, char **argv)
     if (file == NULL)
     {
         printf("Error: Can't open file for write");
-        free(arrWork);
-        free(arrInp);
+
         exit(ERROR_FILE_OPEN_WRITE);
     }
+    // ������ �������
 
-    //file output
     lastPrintElem = arrWork + cntWorkElem;
+
     write_file(file, arrWork, lastPrintElem);
 
     fclose(file);
@@ -181,9 +150,17 @@ int main(int argc, char **argv)
     // ������� ��������
 
     free(arrInp);
-    free(arrWork);
+    if (argc == 4)
+    {
+        if (argv[argc - 1] == 0)
+        {
+            // ��������� ������ ��� ����������
+            free(arrWork);
+        }
+    }
+
+
 
     return 0;
 }
-
 
