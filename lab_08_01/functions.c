d1e9599b991f5570451797db941ad2dcd141d969
Pisaruc Victor <vpisaruc@gmail.com>
2017-12-08 17:28:41 +0300

    Update functions.c

--- lab_08_01/functions.c
+++ lab_08_01/functions.c
@@ -153,64 +153,62 @@ int GausMethod(int row, int column, double **matrix, FILE *file)
 {
     if(row == column - 1)
     {
-        if(row > 1 && column > 2)
-        {    
-            int n = row;
-            int L = 0;
-            double max_el = 0.0;
-            
-            double tmpf = 0.0, tmpf2 = 0.0, t3 = 0.0;
-            for(int j = 0; j < n; j++)
+        int n = row;
+        int L = 0;
+        double max_el = 0.0;
+        
+        double tmpf = 0.0, tmpf2 = 0.0, t3 = 0.0;
+        for(int j = 0; j < n; j++)
+        {
+            for(int h = 0; h < n; h++)
             {
-                for(int h = 0; h < n; h++)
+                if(fabs(matrix[h][j]) > max_el)
                 {
-                    if(fabs(matrix[h][j]) > max_el)
+                    max_el  = fabs(matrix[h][j]);
+                }
+            }
+            for(int k = 1 + L; k < n; k++)
+            {
+                tmpf = matrix[k][j] / matrix[L][j];
+                for (int i = 0; i <= n; i++)
+                {
+                    if (fabs(tmpf) == 0)
                     {
-                        max_el  = fabs(matrix[h][j]);
+                        break;
                     }
+                    tmpf2 = (matrix[L][i] * tmpf);
+                    t3 = matrix[k][i] - tmpf2;
+                    matrix[k][i] = t3;
                 }
-                for(int k = 1 + L; k < n; k++)
-                {
-                    tmpf = matrix[k][j] / matrix[L][j];
-                    for (int i = 0; i <= n; i++)
-                    {
-                        if (fabs(tmpf) == 0)
-                        {
-                            break;
-                        }
-                        tmpf2 = (matrix[L][i] * tmpf);
-                        t3 = matrix[k][i] - tmpf2;
-                        matrix[k][i] = t3;
-                    }
-                }
-                L++;
             }
-            
-            double x[row];
-            int cnt = 0;
-            for (int i = n - 1; i >= 0; i--)
-            {
-                double s = 0;
-                for(int j = n - 1; j > i; j--)
-                {
-                    s += x[j] * matrix[i][j];
-                }
-                x[i] = (matrix[i][n] - s) / matrix[i][i];
-                if (x[i] == 0)
-                {
-                    x[i] = fabs(x[i]);
-                }
-                cnt++;
-            }
-
-            fprintf(file,"%d ", 1);
-            fprintf(file, "%d\n", cnt);
-            for(int i = 0; i < row; i++)
-            {
-                fprintf(file,"%lf\n", x[i]);
-            }
-            return OK;
+            L++;
         }
+        
+        double x[row];
+        int cnt = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            double s = 0;
+            for(int j = n - 1; j > i; j--)
+            {
+                s += x[j] * matrix[i][j];
+            }
+            x[i] = (matrix[i][n] - s) / matrix[i][i];
+            if (x[i] == 0)
+            {
+                x[i] = fabs(x[i]);
+            }
+            cnt++;
+        }
+
+        fprintf(file,"%d ", 1);
+        fprintf(file, "%d\n", cnt);
+        for(int i = 0; i < row; i++)
+        {
+            fprintf(file,"%lf\n", x[i]);
+        }
+        return OK;
+
     }
 
     return GAUS_ERROR;
