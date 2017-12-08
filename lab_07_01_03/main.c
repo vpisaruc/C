864935bb3d7cf9289bebdcf82a3d68d1fe5f3567
Pisaruc Victor <vpisaruc@gmail.com>
2017-12-08 03:54:12 +0300

    Update main.c

--- lab_07_01_03/main.c
+++ lab_07_01_03/main.c
@@ -11,9 +11,7 @@ int main(int argc, char **argv)
     FILE * file = NULL;
     int retVal, cntElem, cntWorkElem;
     int *arrInp = NULL, *arrWork = NULL, *afterLastElem = NULL, *lastPrintElem = NULL, *afterLastElemWork = NULL;
-    //unsigned long long tb_mySort, te_mySort, tb_qSort, te_qSort;
-    
-    
+    int out;
     // �������� ����� c ��������� ������� ��� ������
     if (argc == 4)
     {
@@ -31,147 +29,151 @@ int main(int argc, char **argv)
     if (file == NULL)
     {
         printf("Error: File  not exists");
-        
-
-        exit(ERROR_FILE_NOT_EXISTS);
-    }
-
-    // ������� ���������� ���������
-    retVal = countFileData(file, &cntElem);
-    if (retVal < 0)
-    {
-        // ������� ���� ������
-        printf("Error = %d\n\nInput any key for Exit", retVal);
-        
-
-        exit(retVal);
-    }
-
-    // ��������� ������
-    arrInp = (int*)malloc(cntElem * sizeof(int));
-    if (!arrInp)
-    {
-        printf("\nMemory allocation error\n");
-        exit(MEMMORY_ERROR);
-    }
-    
-    afterLastElem = arrInp + cntElem;
-    arrWork = arrInp;
-    afterLastElemWork = afterLastElem;
-
-    // �������� ������
-    retVal = loadFileData(file, arrInp, &cntElem);
-    if (retVal < 0)
-    {
-        // ������� ���� ������
-        printf("Error = %d\n\nInput any key for Exit", retVal);
-        
-
-        exit(retVal);
-    }
-
-    // �������� �����
-    fclose(file);
-
-    // ������ �������� �������
-    lastPrintElem = afterLastElem;
-    printArray("Input Array", arrInp, lastPrintElem);
-
-    // ���������� � ���������� ������ � ������� ������
-    if (argc == 4)
-    {
-        if (*argv[argc - 1] == 'f')
-        {
-            retVal = key(arrInp, afterLastElem, &arrWork, &afterLastElemWork);
-            if (retVal == MEMMORY_ERROR)
-            {
-                printf("\nMemory error\n");
-                free(arrInp);
-                free(arrWork);
-                exit(MEMMORY_ERROR);
-            }
-            if (retVal == INCORRECT_PARAM)
-            {
-                printf("\nIncorrect parametres\n");
-                free(arrInp);
-                exit(INCORRECT_PARAM);
-            }
-            if (retVal == NONE_ELEMENTS)
-            {
-                printf("\nNone elements before negative\n");
-                free(arrInp);
-                exit(NONE_ELEMENTS);
-            }
-            if (retVal == SIZE_ERROR)
-            {
-                printf("Size error");
-                free(arrInp);
-                free(arrWork);
-                exit(SIZE_ERROR);
-            }
-
-            cntWorkElem = afterLastElemWork - arrWork;
-
-            // ������ ���������������� �������
-            lastPrintElem = arrWork + cntWorkElem;
-            printArray("Filtered Array", arrWork, lastPrintElem);
-        }
-        else
-        {
-            free(arrInp);
-            printf("To use filter print [f]");
-            exit(COMMAND_LINE_ERROR);
-        }
+        out = ERROR_FILE_NOT_EXISTS;
     }
     else
     {
-        // ��� ����������
-        cntWorkElem = cntElem;
-    }
-
-    //Sort array
-    mysort(arrWork, cntWorkElem, sizeof(int), compareFunc);
-
-    // ������ ���������������� �������
-    lastPrintElem = arrWork + cntWorkElem;
-    printArray("Sorting Array", arrWork, lastPrintElem);
-
-    // ������ ������ � ����
-    if (argc == 4)
-    {
-        file = fopen(argv[argc - 2], "w");
-    }
-    else if (argc == 3)
-    {
-        file = fopen(argv[argc - 1], "w");
-    }
-    if (file == NULL)
-    {
-        printf("Error: Can't open file for write");
-        free(arrInp);
-        if (argc == 4)
+        // ������� ���������� ���������
+        retVal = countFileData(file, &cntElem);
+        if (retVal < 0)
         {
-            free(arrWork);
+            // ������� ���� ������
+            printf("Error = %d\n\nInput any key for Exit", retVal);
+            out = retVal;
         }
-        exit(ERROR_FILE_OPEN_WRITE);
-    }
-    // ������ �������
+        else
+        {
+            // ��������� ������
+            arrInp = (int*)malloc(cntElem * sizeof(int));
+            if (!arrInp)
+            {
+                printf("\nMemory allocation error\n");
+                out = MEMMORY_ERROR;
+            }
+            else
+            {
+                afterLastElem = arrInp + cntElem;
+                arrWork = arrInp;
+                afterLastElemWork = afterLastElem;
 
-    lastPrintElem = arrWork + cntWorkElem;
+                // �������� ������
+                retVal = loadFileData(file, arrInp, &cntElem);
+                if (retVal < 0)
+                {
+                    // ������� ���� ������
+                    printf("Error = %d\n\nInput any key for Exit", retVal);
+                    out = retVal;
+                }
+                else
+                {
+                    // �������� �����
+                    fclose(file);
 
-    write_file(file, arrWork, lastPrintElem);
+                    // ������ �������� �������
+                    lastPrintElem = afterLastElem;
+                    printArray("Input Array", arrInp, lastPrintElem);
 
-    fclose(file);
-    // ������� ��������
+                    // ���������� � ���������� ������ � ������� ������
+                    if (argc == 4)
+                    {
+                        if (*argv[argc - 1] == 'f')
+                        {
+                            out = key(arrInp, afterLastElem, &arrWork, &afterLastElemWork);
+                            if (out == MEMMORY_ERROR)
+                            {
+                                printf("\nMemory error\n");
+                                free(arrInp);
+                                free(arrWork);
+                                out = MEMMORY_ERROR;
+                            }
+                            if (out == INCORRECT_PARAM)
+                            {
+                                printf("\nIncorrect parametres\n");
+                                free(arrInp);
+                                out = INCORRECT_PARAM;
+                            }
+                            if (out == NONE_ELEMENTS)
+                            {
+                                printf("\nNone elements before negative\n");
+                                free(arrInp);
+                                out = NONE_ELEMENTS;
+                            }
+                            if (out == SIZE_ERROR)
+                            {
+                                printf("Size error");
+                                free(arrInp);
+                                free(arrWork);
+                                out = SIZE_ERROR;
+                            }
+                            if(out == OK)
+                            {
+                                cntWorkElem = afterLastElemWork - arrWork;
 
-    free(arrInp);
-    if (argc == 4)
-    {
-        free(arrWork);
-    }
+                                // ������ ���������������� �������
+                                lastPrintElem = arrWork + cntWorkElem;
+                                printArray("Filtered Array", arrWork, lastPrintElem);
+                            }
+                        }
+                        else
+                        {
+                            free(arrInp);
+                            printf("To use filter print [f]");
+                            out = COMMAND_LINE_ERROR;
+                        }
+                    }
+                    else
+                    {
+                        // ��� ����������
+                        cntWorkElem = cntElem;
+                    }
 
+                    if(out == OK)
+                    {
+                        //Sort array
+                        mysort(arrWork, cntWorkElem, sizeof(int), compareFunc);
 
+                        // ������ ���������������� �������
+                        lastPrintElem = arrWork + cntWorkElem;
+                        printArray("Sorting Array", arrWork, lastPrintElem);
 
-    return 0;
+                        // ������ ������ � ����
+                        if (argc == 4)
+                        {
+                            file = fopen(argv[argc - 2], "w");
+                        }
+                        else if (argc == 3)
+                        {
+                            file = fopen(argv[argc - 1], "w");
+                        }
+                        if (file == NULL)
+                        {
+                            printf("Error: Can't open file for write");
+                            free(arrInp);
+                            if (argc == 4)
+                            {
+                                free(arrWork);
+                            }
+                            out = ERROR_FILE_NOT_EXISTS;
+                        }
+                        // ������ �������
+
+                        lastPrintElem = arrWork + cntWorkElem;
+
+                        write_file(file, arrWork, lastPrintElem);
+
+                        fclose(file);
+                        // ������� ��������
+
+                        free(arrInp);
+                        if (argc == 4)
+                        {
+                            free(arrWork);
+                        }
+                    }
+                }
+            }
+        }    
+    }       
+    return out;
 }
 
