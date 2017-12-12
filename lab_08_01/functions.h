beae689ac2c0c3cb6c072946f6d42f3b9c70a955
Pisaruc Victor <vpisaruc@gmail.com>
2017-12-12 20:40:09 +0300

    Update functions.h

--- lab_08_01/functions.h
+++ lab_08_01/functions.h
@@ -15,5 +15,5 @@ double**fill_matrix(FILE *file, int *size1, int *size2);
 void print_matrix(double **matrix, int n, int m, int argc);
 double **init(double **matrix, int n, int m);
 double**matrixSumm(int row_1, int column_1, int row_2, int column_2, double **matrix_1, double **matrix_2);
-double**matrixMult (int row_1, int column_1, int row_2, int column_2, double **matrix_1, double **matrix_2);
+double**matrixMult(int row_1, int column_1, int row_2, int column_2, double **matrix_1, double **matrix_2);
 int gausMethod(int row, int column, double **matrix, FILE *file);
